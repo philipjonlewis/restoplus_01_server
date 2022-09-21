907154c7ff3c07daa71974bd153d174b800056bf
Philip Lewis <philipjonlewis@gmail.com>
2022-09-21 08:29:41 +0800

    fixed environment settings

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 export const config = {
-  environment: 'development',
+  environment: process.env.ENVIRONMENT,
   URL: '/restoplus/api_v1',
   port: PORT,
   frontendPort: process.env.FRONTEND_PORT,
